4fe1851a6afb6796dcc3d36a49660c427831e72b
kurtreyn <kurtreyn@yahoo.com>
2022-03-12 15:37:28 -0500

    update port to 3001

--- app.js
+++ app.js
@@ -43,10 +43,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));
+
 
 app.use(passport.initialize());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
